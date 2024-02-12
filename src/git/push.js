63cc8301a37c51bb966f3dc7a38dc98dcd086d79
Fabio Costa <6663243+fab1o@users.noreply.github.com>
2024-02-12 11:44:38 -0500

    Git push update

--- src/git/push.js
+++ src/git/push.js
@@ -2,15 +2,15 @@ const execSync = require('../shell/execSync');
 const getBranchName = require('./getBranchName');
 
 /**
- * @param {String} [branchName=git.getBranchName()]
  * @param {Boolean} [force=false]
+ * @param {String} [branchName=git.getBranchName()]
  * @param {Object} [options] - Shell command options
  * @param {String} [options.cwd]
  * @param {Boolean} [options.dryRun]
  * @desc Push commits to given remote branch
  * @throws {Error} If it does not push the commits
  */
-module.exports = function push(branchName = getBranchName(), force = false, options) {
+module.exports = function push(force = false, branchName = getBranchName(), options) {
     const forceFlag = force ? '--force' : '';
 
     execSync(
