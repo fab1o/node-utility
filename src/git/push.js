98d307df055fbf5a2bf18ae11e243cb04d0f5315
Fabio Costa <6663243+fab1o@users.noreply.github.com>
2024-02-12 15:50:33 -0500

    Move force to options in git push()

--- src/git/push.js
+++ src/git/push.js
@@ -1,16 +1,20 @@
 const execSync = require('../shell/execSync');
+
 const getBranchName = require('./getBranchName');
 
 /**
- * @param {Boolean} [force=false]
+ * @access public
+ * @since 1.0.0
  * @param {String} [branchName=git.getBranchName()]
- * @param {Object} [options] - Shell command options
+ * @param {Object} [options={}] - Shell command options
+ * @param {Boolean} [options.force=false]
  * @param {String} [options.cwd]
  * @param {Boolean} [options.dryRun]
  * @desc Push commits to given remote branch
- * @throws {Error} If it does not push the commits
  */
-module.exports = function push(force = false, branchName = getBranchName(), options) {
+module.exports = function push(branchName = getBranchName(), options) {
+    const { force } = options || {};
+
     const forceFlag = force ? '--force' : '';
 
     execSync(
