c9c464c42b96a1730398149a4e3d8d7239468b64
Fabio Costa <fabio.jsx@gmail.com>
2024-02-12 10:13:13 -0500

    Add execAsync

--- src/shell/execAsync.js
+++ src/shell/execAsync.js
@@ -2,16 +2,16 @@ const process = require('child_process');
 
 /**
  * @access public
- * @since 1.0.0
+ * @since 1.7.0
  * @param {String} command - Command to execute
  * @param {Object} [options={}]
  * @param {String} [options.cwd] - Working path to set git to work in
  * @param {String} [options.dryRun] - Do not run command, only logs them
- * @desc Execute shell commands
+ * @desc Execute shell commands asynchroniously.
  * @returns {String} Output
  * @throws {Error}
  */
-module.exports = function execSync(command, options = {}) {
+module.exports = function execAsync(command, options = {}) {
     const { cwd = '.', dryRun = false } = options;
 
     const execOptions = {
@@ -25,5 +25,13 @@ module.exports = function execSync(command, options = {}) {
         return '';
     }
 
-    return process.execSync(command, execOptions).toString().trim();
+    return new Promise((resolve, reject) => {
+        process.exec(command, execOptions, (error, stdout, stderr) => {
+            if (error) {
+                reject(stdout.trim() || stderr.trim() || error.cmd?.trim());
+            } else {
+                resolve(stdout.trim());
+            }
+        });
+    });
 };
--- src/shell/index.js
+++ src/shell/index.js
@@ -7,15 +7,18 @@
  * @since 1.0.0
  * @typedef {Object} shell - shell
  * @property {Function} cwd - Gets current working directory name.
+ * @property {Function} execAsync - Execute shell commands asynchronously.
  * @property {Function} execSync - Execute shell commands.
  * @property {Function} commandExists - Checks if command exists in PATH.
  */
 const cwd = require('./cwd');
+const execAsync = require('./execAsync');
 const execSync = require('./execSync');
 const commandExists = require('./commandExists');
 
 module.exports = {
     cwd,
+    execAsync,
     execSync,
     commandExists
 };
