6c3eeafbc048f8c858c9d0323bf36fc98e7accbe
Fabio Costa <6663243+fab1o@users.noreply.github.com>
2024-02-12 15:49:11 -0500

    Git branch off branch

--- src/git/branch.js
+++ src/git/branch.js
@@ -2,24 +2,30 @@ const execSync = require('../shell/execSync');
 
 /**
  * @param {String} branchName
- * @param {Boolean} [force=false]
- * @param {Object} [options] - Shell command options
+ * @param {Object} [options={}] - Shell command options
+ * @param {String} [options.baseBranch]
+ * @param {Boolean} [options.force=false]
  * @param {String} [options.cwd]
  * @param {Boolean} [options.dryRun]
  * @desc Creates a branch
  * @throws {Error} If it does not create a branch
  */
-module.exports = function branch(branchName, force = false, options) {
-    // if branch is already created it will throw an error
+module.exports = function branch(branchName, options) {
+    const { baseBranch, force = false } = options || {};
+
     try {
+        if (!baseBranch) {
+            throw new Error('branch() options.baseBranch is required');
+        }
+
         // create a new branch
-        execSync(`git branch ${branchName}`, options);
+        execSync(`git branch ${branchName} ${baseBranch}`, options);
         //
     } catch (ex) {
         if (force) {
             // delete and re-create a new branch
             execSync(`git branch -D ${branchName}`, options);
-            execSync(`git branch ${branchName}`, options);
+            execSync(`git branch ${branchName} ${baseBranch}`, options);
         } else {
             throw ex;
         }
