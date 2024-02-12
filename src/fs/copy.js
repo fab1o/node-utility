f279dc269a1f3fe07553f675b9cc1f8d5f492bf8
Fabio Costa <fabio.jsx@gmail.com>
2024-02-12 10:14:23 -0500

    Add copy (async)

---
+++ src/fs/copy.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * @access public
+ * @since 1.7.0
+ * @param {String} source - Source path to copy from
+ * @param {String} target - Target path to copy to
+ * @param {Boolean} [overwrite=false] - Overwrite files if possible
+ * @desc Copies folders and files from a source path to a target path asynchronously.
+ * @returns {Promise<Boolean>} Returns true if source was copied to target or false otherwise
+ * @throws {Error}
+ */
+module.exports = function copy(source, target, overwrite = false) {
+    source = path.resolve(source);
+    target = path.resolve(target);
+
+    // if source does not exist, nothing to copy
+    if (fs.existsSync(source) === false) {
+        throw new Error(`Source "${source}" does not exist`);
+    }
+
+    // if target exists, do not copy unless overwrite is true
+    if (fs.existsSync(target)) {
+        if (overwrite === false) {
+            return false;
+        }
+    } else {
+        fs.mkdirSync(target);
+    }
+
+    // if they are the same, do not copy unless overwrite is true
+    if (source === target && overwrite === false) {
+        return false;
+    }
+
+    return new Promise((resolve, reject) => {
+        fs.readdir(source, (readErr, files) => {
+            if (readErr) {
+                reject(readErr);
+            } else {
+                Promise.all(
+                    files.map((file) => {
+                        return new Promise((innerResolve, innerReject) => {
+                            const paths = [source, target];
+                            const [srcPath, destPath] = paths.map((p) => path.join(p, file));
+
+                            fs.lstat(srcPath, (statErr, stat) => {
+                                if (statErr) {
+                                    innerReject(statErr);
+                                } else {
+                                    switch (true) {
+                                        case stat.isFile():
+                                            fs.copyFile(
+                                                srcPath,
+                                                destPath,
+                                                fs.constants.COPYFILE_FICLONE,
+                                                (copyErr) => {
+                                                    if (copyErr) {
+                                                        innerReject(copyErr);
+                                                    } else {
+                                                        innerResolve();
+                                                    }
+                                                }
+                                            );
+                                            break;
+                                        case stat.isDirectory():
+                                            copy(srcPath, destPath, overwrite)
+                                                .then(() => innerResolve())
+                                                .catch((err) => innerReject(err));
+                                            break;
+                                        case stat.isSymbolicLink():
+                                            fs.readlink(srcPath, (readLinkErr, linkString) => {
+                                                if (readLinkErr) {
+                                                    innerReject(readLinkErr);
+                                                } else {
+                                                    fs.symlink(
+                                                        linkString,
+                                                        destPath,
+                                                        (symlinkErr) => {
+                                                            if (symlinkErr) {
+                                                                innerReject(symlinkErr);
+                                                            } else {
+                                                                innerResolve();
+                                                            }
+                                                        }
+                                                    );
+                                                }
+                                            });
+                                            break;
+                                        default:
+                                            innerResolve();
+                                            break;
+                                    }
+                                }
+                            });
+                        });
+                    })
+                )
+                    .then(() => resolve(true))
+                    .catch((err) => reject(err));
+            }
+        });
+    });
+};
--- src/fs/index.js
+++ src/fs/index.js
@@ -6,12 +6,14 @@
  * @access public
  * @since 1.0.0
  * @typedef {Object} fs - fs
+ * @property {Function} copy - Copies folders and files from a source path to a target path asynchronously.
  * @property {Function} copySync - Copies folders and files from a source path to a target path.
  * @property {Function} getFolders - Gets folders in a folder.
  * @property {Function} pathExists - Checks if target path exists.
  * @property {Function} deleteFolderSync - Removes a folder synchronously.
  * @property {Function} deleteFolder - Removes a folder asynchronously.
  */
+const copy = require('./copy');
 const copySync = require('./copySync');
 const getFolders = require('./getFolders');
 const pathExists = require('./pathExists');
@@ -19,6 +21,7 @@ const deleteFolderSync = require('./deleteFolderSync');
 const deleteFolder = require('./deleteFolder');
 
 module.exports = {
+    copy,
     copySync,
     getFolders,
     pathExists,
