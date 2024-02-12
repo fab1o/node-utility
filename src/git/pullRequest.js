a2f238b94778a33aa5bfe9c50b4c5bec0c0e861f
Fabio Costa <6663243+fab1o@users.noreply.github.com>
2024-02-12 15:49:32 -0500

    Move shellOptions of pullRequest() in options

--- src/git/pullRequest.js
+++ src/git/pullRequest.js
@@ -3,23 +3,33 @@ const execSync = require('../shell/execSync');
 const string = require('../string');
 
 /**
+ * @access public
+ * @since 1.0.0
  * @param {String} template - PR Template
  * @param {Object} data - Data for PR template
- * @param {Object} [options]
- * @param {String} [options.baseBranch='main'] - PR base branch
+ * @param {Object} [options={}]
+ * @param {String} [options.baseBranch] - PR base branch
  * @param {String} [options.milestone=''] - PR milestone
  * @param {String} [options.labels=''] - PR labels separated by comma
- * @param {String} [options.shellOptions] - Shell command options
- * @param {String} [options.shellOptions.cwd]
- * @param {Boolean} [options.shellOptions.dryRun]
+ * @param {String} [options.cwd]
+ * @param {Boolean} [options.dryRun]
  * @desc Creates a PR
- * @throws {Error}
+ * @throws {Error} If it does not create a PR
  */
 module.exports = function pullRequest(template, data, options) {
-    const { baseBranch = 'main', milestone = '', labels = '', shellOptions } = options || {};
+    const { baseBranch, milestone = '', labels = '', cwd, dryRun } = options || {};
+
+    if (!baseBranch) {
+        throw new Error('pullRequest() options.baseBranch is required');
+    }
 
     // fill in fields on template
     const prDesc = string.replace(template, data);
+
+    const shellOptions = {
+        cwd,
+        dryRun
+    };
 
     // create the pull request
     execSync(
