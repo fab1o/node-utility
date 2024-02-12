c7c653f63ec867212900bd759eb8b288882f3944
Fabio Costa <6663243+fab1o@users.noreply.github.com>
2024-02-12 11:23:23 -0500

    1.8.0

--- src/console/line.test.js
+++ src/console/line.test.js
@@ -1,7 +1,7 @@
 const console = require('./index');
 
 describe('line', () => {
-    it('should checkout branch', () => {
+    it('should print lines', () => {
         const logSpy = jest.spyOn(global.console, 'log');
 
         console.line(10);
