7106763742b3a4889d5c3a001b2603aea1c51449
Fabio Costa <fabio.jsx@gmail.com>
2024-02-12 10:13:42 -0500

    Push tags

--- src/gitSync/push.js
+++ src/gitSync/push.js
@@ -1,15 +1,20 @@
 const execSync = require('../shell/execSync');
+const getBranchName = require('./getBranchName');
 
 /**
- * @param {String} branchName
+ * @param {String} [branchName=git.getBranchName()]
  * @param {Boolean} [force=false]
  * @param {Object} [options] - Shell command options
  * @param {String} [options.cwd]
  * @param {Boolean} [options.dryRun]
  * @desc Push commits to given remote branch
+ * @throws {Error} If it does not push the commits
  */
-module.exports = function push(branchName, force = false, options) {
+module.exports = function push(branchName = getBranchName(), force = false, options) {
     const forceFlag = force ? '--force' : '';
 
-    execSync(`git push --no-verify ${forceFlag} --set-upstream origin ${branchName}`, options);
+    execSync(
+        `git push --tags --no-verify ${forceFlag} --set-upstream origin ${branchName}`,
+        options
+    );
 };
