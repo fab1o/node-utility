2573e4fe2f96a944926e7498e21df76b357972f8
Fabio Costa <6663243+fab1o@users.noreply.github.com>
2024-03-29 09:05:49 -0400

    Prune option on pull

--- src/git/pull.js
+++ src/git/pull.js
@@ -3,10 +3,15 @@ const execSync = require('../shell/execSync');
 /**
  * @access public
  * @param {Object} [options]
+ * @param {Boolean} [options.prune=false]
  * @param {String} [options.cwd]
  * @param {Boolean} [options.dryRun]
  * @desc Pulls latest
  */
 module.exports = function pull(options) {
-    execSync('git pull --quiet --all', options);
+    const { prune = false } = options || {};
+
+    const pruneFlag = prune ? '--prune' : '';
+
+    execSync(`git pull ${pruneFlag} --quiet --all`, options);
 };
